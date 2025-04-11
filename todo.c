ec3ee5d2f6810ee45682b564953e6b37832bbfe4
xaizko <tonytran177013@gmail.com>
2025-04-11 00:18:19 -0400

    First Commmit, added welcome message

---
+++ todo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "color.h"
+
+
+
+int main(void) {
+    printf(BLUE "###########################\n");
+    printf("#Welcome to the Todo List!#\n");
+    printf("###########################\n");
+    
+}
