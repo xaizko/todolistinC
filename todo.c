74a26e944ebfc536a527c937c419cc4ee84a8132
xaizko <tonytran177013@gmail.com>
2025-04-13 22:05:40 -0400

    Added modifying

--- todo.c
+++ todo.c
@@ -7,6 +7,7 @@ void select_choice();
 void react_to_choice(int choice);
 void add_task();
 void view_tasks();
+void modify_task();
 void delete_task();
 // defining file
 FILE *todoFile = NULL;
@@ -39,12 +40,17 @@ void react_to_choice(int choice)
     {
     case 1:
         add_task();
+        //recall choice to keep the process going
         select_choice(choice);
         break;
     case 2:
         view_tasks();
         select_choice(choice);
         break;
+    case 3:
+        modify_task();
+        select_choice(choice);
+        break;  
     case 4:
         delete_task();
         select_choice(choice);
@@ -71,7 +77,7 @@ void add_task()
 
     // Open the file in append mode
     todoFile = fopen("todo.txt", "a");
-    fprintf(todoFile, "%s\n", task);
+    fprintf(todoFile, "%s [NOT COMPLETED]\n", task);
     fclose(todoFile);
 
     // Print success message
@@ -82,28 +88,67 @@ void view_tasks(){
     char task[100];
     int count = 1;
     todoFile = fopen("todo.txt", "r");
+    printf("\n");
     while (fgets(task, sizeof(task), todoFile)) {
-        printf(RED "%d. %s [NOT COMPLETED]", count, task);
+        if (strstr(task, "[DONE]") != NULL) {
+            printf(GREEN "%d. %s", count, task);
+        } else {
+            printf(RED "%d. %s", count, task);
+        }
         count++;
     }
     fclose(todoFile);
 }
 
+void modify_task() {
+    int task_number;
+    char task[100];
+    FILE *todoFile2 = NULL;
+
+    printf(YELLOW "Enter the task number to modify: ");
+    scanf("%d", &task_number);
+
+    //open 2 files, one for reading and one for writing, if the line is not the one to be modified, write it to the new file, clone the files afterwards 
+    todoFile = fopen("todotempt.txt", "w");
+    todoFile2 = fopen("todo.txt", "r");
+    int current_line = 1;
+    while (fgets(task, sizeof(task), todoFile2)) {
+        if (current_line != task_number) {
+            fputs(task, todoFile);
+        } else {
+            char *bracket = strchr(task, '['); // Find the first occurrence of '['
+                int index = bracket - task; // pointer math to find the index needed
+                char temp[100];
+                strncpy(temp, task, index);  
+                temp[index] = '\0'; // need to add null terminator        
+                fprintf(todoFile, "%s[DONE]\n", temp);
+            }
+        current_line++;
+        }
+        
+
+    fclose(todoFile2);
+    fclose(todoFile);
+    //swapping
+    remove("todo.txt");
+    rename("todotempt.txt", "todo.txt");
+
+    //clear random buffer
+    size_t len = strlen(task);
+    if (len > 0 && task[len - 1] == '\n') {
+        task[len - 1] = '\0';
+    }
+
+    printf(GREEN "Task '%s' modified successfully!\n\n", task);
+}
+
 void delete_task() {
     int task_number;
     char task[100];
-    int lines = 0;
     FILE *todoFile2 = NULL;
 
     printf(YELLOW "Enter the task number to delete: ");
     scanf("%d", &task_number);
-
-    // Open the file in read mode and get amount of lines
-    todoFile = fopen("todo.txt", "r");
-    while (fgets(task, sizeof(task), todoFile)) {
-        lines++;
-    }
-    fclose(todoFile);
 
     //open 2 files, one for reading and one for writing, if the line is not the one to be deleted, write it to the new file, clone the files afterwards 
     todoFile = fopen("todotempt.txt", "w");
